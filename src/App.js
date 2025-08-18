4ba0ee5f78b78c17abbea2e85a1d0f497e3f17ef
lukascomka <lukas.souza@sptech.school>
2025-08-18 00:43:39 -0300

    Pagina Principal pronta

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import './App.css';
 
 
+
 import Index_part2 from "./components/conteudo-main_parte2/Index_part2";
 
 import NavComponent from "./components/nav-bar/nav-bar";
 
 import CarrosselMain from "./components/carrossel_home/carrossel_home";
 
+import Footer_Main from './components/footer_main/Footer_Main';
+
+
+
+import instagram from './img/instagram.png'
+import github from './img/github_1051326.png'
+
+import logo_footer from "./img/logo_semFundo.png";
+
 function App() {
   return (
     <div className="container_pagina">
       <NavComponent />
       <CarrosselMain />
-      <Index_part2/>
+      <Index_part2 />
+      <Footer_Main />
+      <div className="roda_pe">
+        <div className="midiaSocial">
+          <img src={instagram} alt="" />
+          <img src={github} alt="" />
+        </div>
+        <h3>company - <span>BITWARE</span></h3>
+        <img src={logo_footer} className="logo_footer" alt="logo" />
+        
+      </div>
 
 
     </div>
---
+++ src/components/footer_main/Footer_Main.jsx
@@ -0,0 +1,16 @@
+import './Footer_Main.css';
+
+function Footer_Main() {
+    return (
+        <div className="footer_main">
+            <div className="conteudo_footerMain">
+                <h1 className="fraseA">Inovação, segurança e gestão,</h1>
+                <h1 className="fraseB">tudo em um só lugar.</h1>
+                <h4 className="fraseC">Controlamos todos os recursos do seu ecossitema de mineração, geramos gráficos com relevância para seu negócio, além de um sistema robusto de governança para seu negócio.</h4>
+                <a className="btnSaibaMaisSession3" href="solucoes.jsx">Saiba Mais</a>
+            </div>
+
+        </div>
+        );
+}
+export default Footer_Main;
