249a14a3f724ab7a2584178225a1643888c24b03
lukascomka <lukas.souza@sptech.school>
2025-08-18 01:02:08 -0300

    organizacao de caminho

--- src/App.js
+++ src/App.js
@@ -10,12 +10,9 @@ import CarrosselMain from "./components/carrossel_home/carrossel_home";
 
 import Footer_Main from './components/footer_main/Footer_Main';
 
+import Roda_pe from '../src/components/roda_pe/Roda_pe';
 
 
-import instagram from './img/instagram.png'
-import github from './img/github_1051326.png'
-
-import logo_footer from "./img/logo_semFundo.png";
 
 function App() {
   return (
@@ -24,15 +21,7 @@ function App() {
       <CarrosselMain />
       <Index_part2 />
       <Footer_Main />
-      <div className="roda_pe">
-        <div className="midiaSocial">
-          <img src={instagram} alt="" />
-          <img src={github} alt="" />
-        </div>
-        <h3>company - <span>BITWARE</span></h3>
-        <img src={logo_footer} className="logo_footer" alt="logo" />
-        
-      </div>
+     <Roda_pe />
 
 
     </div>
---
+++ src/components/roda_pe/Roda_pe.jsx
@@ -0,0 +1,24 @@
+import instagram from '/home/lukascomka/Área de trabalho/CCOA-2Semestre/Projeto Integrado segundo semestre/Prototipo_site/bitcoin_pessoal/src/img/instagram.png'
+
+import github from '/home/lukascomka/Área de trabalho/CCOA-2Semestre/Projeto Integrado segundo semestre/Prototipo_site/bitcoin_pessoal/src/img/github_1051326.png'
+
+import logo_footer from "/home/lukascomka/Área de trabalho/CCOA-2Semestre/Projeto Integrado segundo semestre/Prototipo_site/bitcoin_pessoal/src/img/logo_semFundo.png";
+
+import "../roda_pe/Roda_pe.css"
+
+
+function Roda_pe(){
+    return(
+
+         <div className="roda_pe">
+        <div className="midiaSocial">
+          <img src={instagram} alt="" />
+          <img src={github} alt="" />
+        </div>
+        <h3>company - <span>BITWARE</span></h3>
+        <img src={logo_footer} className="logo_footer" alt="logo" />
+        
+      </div>
+    );
+}
+export default Roda_pe;
